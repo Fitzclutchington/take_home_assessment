d8d332871564d63d9bf97b8e62fe13a18dcaaa0b
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2024-09-26 16:02:02 -0400

    add additional report objects

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Optional, Union
 from datetime import datetime
 from blackbox import parse_string_datetime, SnowflakeConnection
@@ -17,7 +17,8 @@ class TimeRange:
     start: FixedTime
     end: FixedTime
 
-
+# Base class to handle inserting into DB and shared validation logic
+# e.g, report range check
 @dataclass
 class ReportSpec:
     report_type: str
@@ -39,3 +40,52 @@ class ReportSpec:
             self.include_brands,
         )
 
+    def validate(self):
+        if len(self.report_ranges) == 0:
+            ValueError("At least one report range is required")
+
+
+# subclasses handle individual reports' validation
+@dataclass
+class TypeIReportSpec(ReportSpec):
+    report_type: str = "Type I"
+    data_source: str = "SYNDICATED"
+    model_group: str
+    outcomes: List[str] = field(default_factory=lambda: ["site_visits", "product_searches"])
+    def validate(self):
+        super().validate()
+        if not self.model_group:
+            raise ValueError("Type I reports require a model group")
+
+
+@dataclass
+class TypeIIReportSpec(ReportSpec):
+    report_type: str = "Type II"
+    data_source: str
+    model_group: str
+    outcomes: List[str] = field(default_factory=lambda: ["site_visits", "product_searches"])
+
+    def validate(self):
+        super().validate()
+        if not self.model_group:
+            raise ValueError("Type II reports require a model group")
+        if not self.data_source:
+            raise ValueError("Type II reports require a data source")
+
+
+@dataclass
+class TypeIIIReportSpec(ReportSpec):
+    report_type: str = "Type III"
+    data_source: str
+    model_group: str = None
+    include_brands: List[str]
+    outcomes: List[str] 
+
+    def validate(self):
+        super().validate()
+        if not self.model_group:
+            raise ValueError("Type III reports require a model group")
+        if not self.data_source:
+            raise ValueError("Type III reports require a data source")
+        if not self.include_brands:
+            raise ValueError("At least one brand must be included for Type III reports.")
